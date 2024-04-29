ddf992a617eb7be34dfd27080b00a467178c6502
Sandy1005 <168471614+Sandy1005@users.noreply.github.com>
2024-04-30 00:32:10 +0530

    Add files via upload

---
+++ src/components/LoveLetter/LoveLetter.js
@@ -0,0 +1,48 @@
+import React, { useState, useRef } from 'react';
+import './LoveLetter.css';
+import audioFile from './kushi.mp3';
+
+const LoveLetter = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isFullSize, setIsFullSize] = useState(false);
+  const audioRef = useRef(null);
+
+  const handleOpenLetter = () => {
+    setIsOpen(true);
+    setTimeout(() => {
+      setIsFullSize(true);
+      // Ensuring audio play is directly a result of this user interaction
+      if (audioRef.current) {
+        audioRef.current.play()
+          .then(() => console.log("Playback succeeded"))
+          .catch(e => console.error("Playback failed:", e));
+      }
+    }, 800);
+  };
+
+  const handleCloseLetter = () => {
+    setIsFullSize(false);
+    setTimeout(() => {
+      if (audioRef.current) {
+        audioRef.current.pause();
+      }
+      setIsOpen(false);
+    }, 800);
+  };
+
+  return (
+    <div className={`envelope ${isOpen ? 'open' : ''}`} onClick={!isFullSize ? handleOpenLetter : handleCloseLetter}>
+      <div className="flap"></div>
+      <div className="body"></div>
+      <div className={`letter ${isFullSize ? 'fullSize' : ''}`}>
+        bujji🖤,<br />
+        naa priyathi priyamaina na paddu nv ante naku calla iestam yantta ani adigitha chappalenu kani iestam ante manna madya lo yani godavallu vachina nenu ninnu vadhullukolenu yantta mandhi girls una kani na eyes and na heart matram nekosam ey yadhurustu unttumdhi<br />i loveeee youuuu :)<br />
+        always yours,<br />
+        @sandy
+      </div>
+      <audio ref={audioRef} src={audioFile} onError={(e) => console.error('Audio error:', e.message)} />
+    </div>
+  );
+};
+
+export default LoveLetter;
